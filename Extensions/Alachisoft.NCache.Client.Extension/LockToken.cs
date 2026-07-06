0fd1013d2461cff8c0047eed60fef1c41897c86c
Suleman Khan <suleman_haroon@diya-tech.com>
2026-07-06 15:47:52 +0500

    add client.extensions (#9)

---
+++ Extensions/Alachisoft.NCache.Client.Extension/LockToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alachisoft.NCache.Client.Extension
+{
+    [Serializable]
+    public class LockToken
+    {
+        public Guid Id { get; set; }
+
+        public LockToken()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool IsEqual(LockToken compLockToken)
+        {
+            return this.Id.Equals(compLockToken.Id);
+        }
+    }
+}
