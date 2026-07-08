c6f7dbc2e9dd2a432c3f88237f96e855bcbdaa90
DIYATECH\ashar_malik <ashar_malik@diya-tech.com>
2026-07-08 14:06:11 +0500

    Restructured repo and added ASP.NET OutputCache sample project

---
+++ AspNet.OutputCacheProvider/AspNetOutputCache_TestApp/AspNetOutputCache_TestApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace AspNetOutputCache_TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration = 10, VaryByParam = "none")]
+        public ActionResult Index()
+        {
+            return Content("Cached at: " + DateTime.Now.ToString());
+        }
+
+        [OutputCache(CacheProfile = "DefaultCache")]
+        public ActionResult Profiled()
+        {
+            return Content("Profile cached at: " + DateTime.Now.ToString());
+        }
+    }
+}
---
+++ AspNet.OutputCacheProvider/AspNetOutputCache_TestApp/AspNetOutputCache_TestApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetOutputCache_TestApp.MvcApplication" Language="C#" %>
