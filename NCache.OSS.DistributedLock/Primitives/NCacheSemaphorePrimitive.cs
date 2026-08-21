d7010fb216518bdfbb4c86627d101fe9c821249d
DIYATECH\ashar_malik <ashar_malik@diya-tech.com>
2026-08-21 14:38:24 +0500

    Add NCache distributed lock integration

---
+++ NCache.OSS.DistributedLock/Primitives/NCacheSemaphorePrimitive.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCache.DistributedLock.Primitives
+{
+    [Serializable]
+    internal class NCacheSemaphorePrimitive
+    {
+        public int Capacity { get; set; }
+
+        public List<DistributedLockAcquisition> DistributedLockAcquisitions { get; set; }
+
+        // Required by many serializers
+        public NCacheSemaphorePrimitive()
+        {
+            DistributedLockAcquisitions = new List<DistributedLockAcquisition>();
+        }
+
+        public NCacheSemaphorePrimitive(int capacity)
+        {
+            Capacity = capacity;
+            DistributedLockAcquisitions = new List<DistributedLockAcquisition>();
+        }
+
+        public List<DistributedLockAcquisition> GetAcquisitions()
+        {
+            return DistributedLockAcquisitions;
+        }
+
+        public bool TryAcquire(DistributedLockAcquisition lockAcquisition)
+        {
+            if (DistributedLockAcquisitions.Count < Capacity)
+            {
+                DistributedLockAcquisitions.Add(lockAcquisition);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Release(DistributedLockAcquisition lockAcquisition)
+        {
+            int removed = DistributedLockAcquisitions.RemoveAll(a => a.IsEqual(lockAcquisition));
+            return removed > 0;
+        }
+
+        public bool RemoveAcquisition(string acquisitionName)
+        {
+            int removed = DistributedLockAcquisitions.RemoveAll(a => a.IsEqual(acquisitionName));
+            return removed > 0;
+        }
+    }
+}
